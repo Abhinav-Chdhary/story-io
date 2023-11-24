8032e1699aedb1e5b624bd25a7ff3f56b5ee7eeb
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-24 13:39:34 +0530

    Title bar and homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import TitleBar from "./components/TitleBar";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <TitleBar />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/components/TitleBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TitleBar() {
+  return (
+    <div className="bg-blue-700 text-white p-4 fixed top-0 left-0">
+      <h1 className="text-2xl font-bold">Story.io</h1>
+    </div>
+  );
+}
